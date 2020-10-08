14238685de459f83b2c2b4e89dd51a156bb7eb6b
Ryan Hagerty <ryanhagerty29@gmail.com>
2020-10-07 23:02:36 -0700

    Connected app to MongoDB database

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private EventRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -30,4 +34,14 @@ public class Application {
 		};
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Sample code for repo
+		repo.deleteAll();
+
+		// Add events
+		repo.save(new Event("New Club Workshop", "2020-10-08T10:00:00", "Office of Student Life and Cultural Centers", "Zoom", "Club & Organization registration information for students seeking to establish new clubs on campus. No RSVP required. If you arrive late you may not be admitted. Zoom link provided here on the date of the programming."));
+		repo.save(new Event("Issa's Eco-Corner", "2020-10-08T11:00:00", "Womxn's Resource Center", "Zoom", "top by and chat with Issa about how to live more \"zero waste\", learn about the environment, DIY fruit fridge masks and more! During Issa's Eco-Corner we will be covering a variety of environmental-related topics, activities, and themes. This week's topic will be \"zero waste kits\" on every budget. Issa will be on Zoom ready to answer any questions you have in order to switch over to more eco-friendly products as well as having a free zero waste kit guide PDF."));
+		repo.save(new Event("Meet the Latinx Librarians & Staff", "2020-10-08T11:30:00", "University Library", "Online", "Did you know that the CPP Library has Latinx/Hispanic Librarians & Staff?  If you have ever asked this question or wondered how librarians landed their jobs, you're not alone. Get to know your librarians, who they are, how they can help, but most importantly their background and how their journey to the CPP Library! This community dialogue will feature the wonderful Latinx/Hispanic Librarians and attendees will learn more about who they are as people as well as how they support students."));
+	}
 }
