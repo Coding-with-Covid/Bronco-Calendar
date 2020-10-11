887b41b9d6c484e780f1dd338aab59947dfd1049
Duc Truong <70175176+dhtruong@users.noreply.github.com>
2020-10-10 20:57:20 -0700

    Added HtmlUnit test case for A6

--- src/test/java/com/example/springboot/concatenation/ConcatenationTest.java
+++ src/test/java/com/example/springboot/concatenation/ConcatenationTest.java
@@ -1,6 +1,9 @@
 package com.example.springboot.concatenation;
 
 import com.example.springboot.Concatenation;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
@@ -28,4 +31,15 @@ public class ConcatenationTest {
 		Concatenation c = new Concatenation();
 		assertTrue(c.compareString("2020","2020"));
 	}
+	
+	@Test
+    public void testWebPage() throws Exception {
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("https://www.events.ucla.edu/");
+            Assert.assertEquals("Events |", page.getTitleText());
+
+            final String pageAsText = page.asText();
+            Assert.assertTrue(pageAsText.contains("The UCLA Events Office schedules and manages student extracurricular activities"));
+        }
+    }
 }
