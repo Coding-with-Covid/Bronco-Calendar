89307e721c49bd770385d74a7501a88728687326
Duc Truong <70175176+dhtruong@users.noreply.github.com>
2020-10-25 19:32:37 -0700

    Added login page

--- src/main/java/com/example/springboot/HomeController.java
+++ src/main/java/com/example/springboot/HomeController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	 @RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
     public String testHome() { return "home.html"; }
 
     @RequestMapping(value = "/events", method = RequestMethod.GET)
     public String testEvents() {
         return "events.html";
     }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String testLogin() { return "login.html"; }
 }
