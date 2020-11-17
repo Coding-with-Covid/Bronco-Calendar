55aa5ff16c572983541981bdcbc06731de9e4569
srpaul53 <samrpaul@gmail.com>
2020-11-17 15:25:10 -0800

    Added permission for anonymous users to access events page

--- src/main/java/com/example/springboot/WebSecurityConfiguration.java
+++ src/main/java/com/example/springboot/WebSecurityConfiguration.java
@@ -11,7 +11,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	    public void configure(HttpSecurity httpSecurity) throws Exception {
 	        httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
 	                .antMatchers("/","/calender", "/login", "welcome").authenticated()
-	                .antMatchers("/home").permitAll()
+	                .antMatchers("/home", "/events", "/").permitAll()
 	                .anyRequest().authenticated()
 	                .and()
 	                .oauth2Login().permitAll()
